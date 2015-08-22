a57e51b9971a2bba6b72bb97fadb4ecea8365809
Greg Coonrod <greg.coonrod@gmail.com>
2015-08-22 18:18:15 -0500

    gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp'),
+    util = require('gulp-util'),
+    jshint = require('gulp-jshint'),
+    beautify = require('gulp-jsbeautifier');
+
+var src_files = './**/*.js';
+
+gulp.task('default', ['watch']);
+
+gulp.task('jshint', function(){
+    return gulp.src(src_files)
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('format-js', function(){
+    return gulp.src(src_files)
+        .pipe(beautify({mode: 'VERIFY_AND_WRITE'}));
+});
+
+gulp.task('watch', function(){
+    return gulp.watch(src_files, ['jshint', 'format-js']);
+});
